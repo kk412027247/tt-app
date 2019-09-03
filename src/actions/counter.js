69dc03caf60f23b7aa3e6568740f5686686b6de5
tmd <kk412027247@gmail.com>
2019-09-03 18:33:52 +0800

    first commit

---
+++ src/actions/counter.js
@@ -0,0 +1,24 @@
+import '@tarojs/async-await'
+
+import {
+  ADD,
+  MINUS
+} from '../constants/counter'
+
+export const add = () => {
+  return {
+    type: ADD
+  }
+}
+export const minus = () => {
+  return {
+    type: MINUS
+  }
+}
+
+export const asyncAdd = () => async (dispatch)=> {
+  setTimeout(() => {
+    dispatch(add())
+  }, 2000)
+};
+
